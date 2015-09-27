d98b41633bf6b78158934587f867adc3e97a4062
mdoxey06 <mdoxey06@gmail.com>
2015-09-26 22:00:22 -0400

    modified listing model with validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,22 @@
 class Listing < ActiveRecord::Base
+	attr_accessible :title, :isbn, :condition, :price, :seller_email, :comments, :course_name
+
+	validates :title, :isbn, :condition, :price, :seller_email, :course_name, :presence => true
+	validates :course_name, :length => { :in => 7..8, :message => " please format course name with 
+		4 letters of department name followed by 3 numbers, possibly with a letter at the end
+		 (ex: CMSC216, MATH141, HONR228E)." }
+
+	require 'net/http'
+	url = 'http://daemondash.bookholders.com/classdata/umcp/'
+	begin
+		resp = Net::HTTP.get_response(URI.parse(url + course_name[0..3] + "/" + course_name))
+	rescue
+		print "Connection error."
+	end
+	resp_text = resp.body
+
+	validates :resp_text["nodata"], exclusion: { in: "yes", :message => " is not a valid course name or format. 
+		Correct format is 4 letters of department name followed by 3 numbers, possibly with a letter at the end
+		 (ex: CMSC216, MATH141, HONR228E)." }
+
 end
