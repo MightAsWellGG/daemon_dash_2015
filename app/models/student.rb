7b999574d0c2ac600e9d4c383d7d149e1765305f
mdoxey06 <mdoxey06@gmail.com>
2015-09-26 23:29:54 -0400

    fixed password variable mess up

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,15 +1,15 @@
 class Student < ActiveRecord::Base
-	attr_accessible :name, :email, :password
+	attr_accessible :name, :email, :p_hash
 
-	validates :pw, :presence => true
-	validates :pw, :length => { :in => 6..25}
+	validates :p_hash, :presence => true
+	validates :p_hash, :length => { :in => 6..25}
 	validates :name, :email, :presence => true
 	validates :email, :uniqueness => { :case_sensitive => false}
 	validates :email, :format => { :with => /@/, :message => " is invalid, please put in valid email with @ symbol!"}
-	validates :email, :format => { :with => "umd.edu", :message => " is invalid, need a UMD domain (umd.edu)"}
+	validates :email, :format => { :with => /umd.edu/, :message => " is invalid, need a UMD domain (umd.edu)"}
 
 	def password
-		password ? @pw ||= BCrypt::Password.new(password) : nil
+		password ? @pw ||= BCrypt::Password.new(p_hash) : nil
 	end
 
 	def password=(new_password)
